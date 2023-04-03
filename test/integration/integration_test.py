0c3aa0988802f53c24c74380a514ee57b7d1c0df
Markus Petke <MP91@users.noreply.github.com>
2023-04-03 07:39:04 +0200

    fix review findings

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -6,8 +6,7 @@ command: str = "velocitas exec runtime-local"
 regex_dapr_app: Pattern[str] = compile(r".*You\'re up and running! Both Dapr and your app logs will appear here\.\n")
 regex_mqtt: Pattern[str] = compile(r".*mosquitto version \d+\.\d+\.\d+ running\n")
 regex_vdb: Pattern[str] = compile(r".*Listening on \d+\.\d+\.\d+\.\d+:\d+")
-regex_fc: Pattern[str] = compile(r".*dbcfeeder: Connected to data broker")
-regex_vs: Pattern[str]= compile(r".*DataBrokerFeederImpl: connected to data broker\.")
+regex_client: Pattern[str]= compile(r".*[cC]onnected to data broker.*")
 timeout_sec: float = 180
 
 
@@ -19,6 +18,9 @@ def run_command_until_logs_match(command: str, regex_service: Pattern[str], run_
     timer: Timer = Timer(timeout_sec, proc.kill)
     timer.start()
     for line in iter(proc.stdout.readline, b""):
+        if proc.poll() is not None:
+            print(f"Timeout reached after {timeout_sec} seconds, process killed!")
+            return False
         print(line)
         if run_with_dapr and regex_dapr_app.search(line):
             successfully_running_dapr = True
@@ -27,16 +29,12 @@ def run_command_until_logs_match(command: str, regex_service: Pattern[str], run_
         if successfully_running and successfully_running_dapr:
             timer.cancel()
             break
-    if proc.poll() is not None:
-        print(f"Timeout reached after {timeout_sec} seconds, process killed!")
-        print(f"No match for Regex: {regex_service}")
-        return False
-    return successfully_running_dapr and successfully_running
+    return True
 
 
 def test_scripts_run_successfully():
     assert 0 == check_call(f"{command} ensure-dapr", shell=True)
     assert run_command_until_logs_match(f"{command} run-mosquitto", regex_mqtt)
     assert run_command_until_logs_match(f"{command} run-vehicledatabroker", regex_vdb, True)
-    assert run_command_until_logs_match(f"{command} run-feedercan", regex_fc, True)
-    assert run_command_until_logs_match(f"{command} run-vehicleservices", regex_vs, True)
+    assert run_command_until_logs_match(f"{command} run-feedercan", regex_client, True)
+    assert run_command_until_logs_match(f"{command} run-vehicleservices", regex_client, True)
